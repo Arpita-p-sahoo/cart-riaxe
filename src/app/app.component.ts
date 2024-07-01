6693888adf9471de15e873cdb25acc100ce2a85d
Arpita.s <arpita.sahoo637174@gail.com>
2024-07-01 20:50:34 +0530

    riaxe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,83 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cart';
+
+  totalPrice:number = 0;
+  groceryItems = [
+    {
+      image: 'https://via.placeholder.com/150',
+      name: 'Apple',
+      price: 30,
+      origin_price: 30,
+      description: 'Lorem, ipsum dolo',
+      quantity: 1
+    },
+    {
+      image: 'https://via.placeholder.com/150',
+      name: 'Banana',
+      price: 40,
+      origin_price: 40,
+      description: 'Lorem, ipsum dolo',
+      quantity: 1
+    },
+    {
+      image: 'https://via.placeholder.com/150',
+      name: 'Carrot',
+      price: 20,
+      origin_price: 20,
+      description: 'Lorem, ipsum dolo',
+      quantity: 1
+    },
+    {
+      image: 'https://via.placeholder.com/150',
+      name: 'Carrot',
+      price: 20,
+      origin_price: 20,
+      description: 'Lorem, ipsum dolo',
+      quantity: 1
+    }
+  ];
+secondpart: boolean = false;
+
+  increaseQuantity(item: any) {
+    item.quantity++;
+
+    item.price = item.quantity * item.origin_price;
+  }
+
+  decreaseQuantity(item: any) {
+    if (item.quantity > 1) {
+      item.quantity--;
+      item.price = item.quantity * item.origin_price;
+
+    }
+  }
+  
+
+  getTotalPrice(): number {
+    let totalPrice = 0;
+    for (let item of this.groceryItems) {
+      totalPrice += item.price * item.quantity;
+    }
+    return totalPrice;
+  }
+  view(){
+    this.secondpart = !this.secondpart;
+    
+  }
+
+  updateQuantity(item: any, event: any) {
+    const inputValue = event.target.value;
+    const Quntity = parseInt(inputValue, 10); 
+
+    if (!isNaN(Quntity) && Quntity > 0) {
+      item.quantity = Quntity;
+    } else {
+      item.quantity = 1;
+    }
+  }
+
+  Total(item: any): number {
+    return item.price * item.quantity;
+  }
 }
